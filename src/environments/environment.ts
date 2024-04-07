0f5cb21d75cea7edcdc41a5aac3e8ddbe602649f
Cindy G <guzmancindy11@gmail.com>
2024-04-06 21:57:00 -0400

    last changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   production: false,
 
   firebaseConfig: {
-    apiKey: "AIzaSyD9kEPh2E5nINt9FeEejiYKgeeLXOwozfM",
-    authDomain: "product-admin-app-2a687.firebaseapp.com",
-    projectId: "product-admin-app-2a687",
-    storageBucket: "product-admin-app-2a687.appspot.com",
-    messagingSenderId: "947746004432",
-    appId: "1:947746004432:web:5bcd24538630c7e94fedd5"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 
 };
